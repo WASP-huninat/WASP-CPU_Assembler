7e0a4d4ba260b2c2d8783340f67a9d5596b3640f
WASP-huninat <123555748+WASP-huninat@users.noreply.github.com>
2023-04-04 12:46:34 +0200

    v0.3.1-alpha

--- WASP-CPU_Assembler/Compiler/wasp/syntax/Gen2Syntax.java
+++ WASP-CPU_Assembler/Compiler/wasp/syntax/Gen2Syntax.java
@@ -13,19 +13,20 @@ public class Gen2Syntax {
 		//if(OP.equals(""))	return I = "";
 		
 	// Jumps
-		if(OP.equals("JAU"))	return I = reg + " 000 0001 0001";
+		if(OP.equals("JAU"))	return I = reg + " 000 0001 0000";
 		if(OP.equals("JAZ"))	return I = reg + " 000 0001 0010";
 		if(OP.equals("JAN"))	return I = reg + " 000 0001 0011";
 		if(OP.equals("JAP"))	return I = reg + " 000 0001 0100";
 		if(OP.equals("JAC"))	return I = reg + " 000 0001 0101";
 	// Load REGX/REGY
-		if(OP.equals("LDI"))	return I = reg + " 0010 0001";
-		if(OP.equals("WAI"))	return I = reg + "1 0010 0010";
-		if(OP.equals("LDX"))	return I = reg + "0 0010 0011";
-		if(OP.equals("LDY"))	return I = reg + "0 0010 0100";
-		if(OP.equals("RFR"))	return I = "0" + reg + " 0010 0101";
-		if(OP.equals("WTR"))	return I = "1" + reg + " 0010 0110";
-		if(OP.equals("GIO"))	return I = reg + " 0010 0111";
+		if(OP.equals("LDI"))	return I = reg + " 1000 1000";
+		if(OP.equals("WIA"))	return I = "0001 " + reg + " 0010 0000";
+		if(OP.equals("LDX"))	return I = "0000 " + reg + "0 0010 0001";
+		if(OP.equals("LDY"))	return I = "0000 " + reg + "0 0010 0010";
+		if(OP.equals("RDI"))	return I = reg + " 0010 0011";
+		if(OP.equals("WDO"))	return I = reg + " 0010 0100";
+		if(OP.equals("WAO"))	return I = reg + " 0010 0101";
+		if(OP.equals("GIO"))	return I = reg + " 0010 0110";
 	// ALU Operations
 		if(OP.equals("ADD"))	return I = "0001 " + reg + " 0100 0001";
 		if(OP.equals("SUB"))	return I = "0001 " + reg + " 0100 0010";
@@ -41,7 +42,6 @@ public class Gen2Syntax {
 		if(OP.equals("BRS"))	return I = "0001 " + reg + " 0100 1100";
 	// I/O and RAM
 		//if(OP.equals("RUI"))	return I = 
-		//if(OP.equals("STD"))	return I = 
 		//if(OP.equals("CRP"))	return I = 
 		else OutTextAreaName.append("no matching instruction found");
 		return I;
